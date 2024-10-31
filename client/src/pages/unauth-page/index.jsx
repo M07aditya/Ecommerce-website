3dda9916243a20f40d564cc5c478a4fb535208ac
M07aditya <2022pcecsaditya007@poornima.org>
2024-10-31 13:24:23 +0530

    minor

--- client/src/pages/unauth-page/index.jsx
+++ client/src/pages/unauth-page/index.jsx
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 function UnauthPage() {
   return (
     <div>
-      <h1>You don't have access to view this page</h1>
+      <h1>You do not have access to view this page</h1>
       <Link to="/auth/login">Go to Login</Link>
     </div>
   );
